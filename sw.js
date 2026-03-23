cd40eef18260c66ec15282caaad3ca3d11e6c705
correo11011correo-netizen <correo11011correo@gmail.com>
2026-03-23 03:02:54 +0000

    PWA Final: Score 100 Fix

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'stockpro-v6';
-const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo.svg'];
+const CACHE_NAME = 'stockpro-v10';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './maskable-icon.png',
+  './logo.svg'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request).then(response => {
-        return response || caches.match('./index.html');
-      });
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => caches.match('./index.html'));
     })
   );
-});
-
-self.addEventListener('sync', event => {
-  if (event.tag === 'sync-stock') {
-    console.log('Sincronización en segundo plano activa');
-  }
 });
